79cb99aa3eb25c853f4a107edf9e5f916b3d40f6
Ligeo <ligeo@yandex.ru>
2013-02-12 21:42:38 +0400

    New commit after reboot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medium::Application.config.secret_token = 'c645993cf39a72ddfd9f8ef5929030d3506e1a327a6f2d7652163f422d379386f6435399b9dc448b0a6479b3f12c2267b42654c7db79a3d689f8e31ab065d217'
